83ed44f0bd343ef8077e0fb63596c51ad5117459
manueldavalo <elmanu014@gamil.com>
2023-09-27 14:50:45 -0300

    pasar archivos .js version1

---
+++ script.js
@@ -0,0 +1,55 @@
+alert("Hello world")
+
+
+let alumno = "manuel" //variable 
+
+const entidad = "Gobierno de Buenos Aires"
+
+console.log(  "el nombre del alumno es " + alumno + "y su nota es 10") //la imprime en el browser
+
+
+// VA COMO PIÑA
+let nombreApellido = "juan"
+let NombreApellido = "nombre"
+let nombre_apellido = "apellido"
+let $user = "usuario"
+let _usuario = "usuario"
+
+
+
+//No VAN
+/*
+let 1er nombre= 
+let nombre-nombre = 
+let var super breack = */
+
+//Tipos de datos de Javascript
+
+//String
+let str1 = "Juans"
+let str2 = 'Pedros'
+let str3 = 'Ramiros'
+
+let str5 = `${str1} y ${str2} y ${str3}`    
+let str4 = (str1 + " y " + str2 + " y " + str3)
+
+console.log(str4)
+console.log(str5)
+
+//Number
+let num1 = parseFloat(prompt("mete el primer numero"))
+let num2 = parseFloat(prompt("mete el segundo numero"))
+let num3 = 3.14223423
+let num4 = 4
+
+let suma = num1 + num4
+let resta = num1 - num4
+let mult = num1 * num4
+let division = num1 / num4
+
+console.log("la suma es: "+suma)
+console.log("la resta es: "+resta)
+console.log("la multiplicacion es: "+mult)
+console.log("la divisiones es: "+division)
+ 
+document.write("<h1>Javascript</h1>")
